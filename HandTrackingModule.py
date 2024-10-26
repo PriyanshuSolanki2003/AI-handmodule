22e341d83b7337acdae91010c5ef8c8844c98e3c
Priyanshu Solanki <uniquestories.abc@gmail.com>
2024-10-26 18:34:32 +0530

    first commit

---
+++ HandTrackingModule.py
@@ -0,0 +1,73 @@
+import cv2
+import mediapipe as mp
+
+class handDetector:
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode,
+                                        max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectionCon,
+                                        min_tracking_confidence=self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.lmList = []
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def findPosition(self, img, handNo=0, draw=True):
+        self.lmList = []
+        bbox = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmList.append([id, cx, cy])
+                if len(bbox) == 0:
+                    bbox = [cx, cy, cx, cy]
+                else:
+                    bbox = [min(bbox[0], cx), min(bbox[1], cy), max(bbox[2], cx), max(bbox[3], cy)]
+            if draw:
+                cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20), (bbox[2] + 20, bbox[3] + 20), (0, 255, 0), 2)
+        return self.lmList, bbox
+
+    def fingersUp(self):
+        fingers = []
+        if not self.lmList:
+            return fingers
+       
+        if self.lmList[4][1] > self.lmList[3][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+    
+        for id in range(1, 5):
+            if self.lmList[4 * id][2] < self.lmList[4 * id - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
+
+    def findDistance(self, p1, p2, img, draw=True):
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+        length = ((x2 - x1) ** 2 + (y1 - y2) ** 2) ** 0.5
+        return length, img, [x1, y1, x2, y2, cx, cy]
